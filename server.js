9cd2a3ca56fd4e70326ca2dbf3cccf5679ba1811
Tharindu Sandaruwan <sandaruwan@liveroom.xyz>
2018-11-01 17:41:48 +0530

    ss

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://178.128.96.45:27017/todo');
+mongoose.connect('mongodb://localhost:27017/todo');
 
 
 
